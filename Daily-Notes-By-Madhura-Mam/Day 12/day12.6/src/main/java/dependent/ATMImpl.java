eb815c10d10a27166a24b3d72f2a711d5757e76f
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-06-27 21:03:43 +0530

    Add files via upload

---
+++ Daily-Notes-By-Madhura-Mam/Day 12/day12.6/src/main/java/dependent/ATMImpl.java
@@ -0,0 +1,38 @@
+package dependent;
+
+import dependency.Transport;
+
+public class ATMImpl implements ATM {
+	private Transport myTransport;
+
+//constr based DI
+	public ATMImpl(Transport t1234) {
+		myTransport=t1234;
+		System.out.println("in cnstr of " + getClass().getName() + " " + myTransport);// not null !
+	}
+
+	@Override
+	public void deposit(double amt) {
+		System.out.println("depositing " + amt);
+		byte[] data = ("depositing " + amt).getBytes();
+		myTransport.informBank(data);
+
+	}
+
+	@Override
+	public void withdraw(double amt) {
+		System.out.println("withdrawing " + amt);
+		byte[] data = ("withdrawing " + amt).getBytes();
+		myTransport.informBank(data);
+	}
+
+	// custom init method
+	public void myInit() {
+		System.out.println("in init " + myTransport);// not null
+	}
+
+	// custom destroy method
+	public void myDestroy() {
+		System.out.println("in destroy " + myTransport);// not null
+	}
+}
