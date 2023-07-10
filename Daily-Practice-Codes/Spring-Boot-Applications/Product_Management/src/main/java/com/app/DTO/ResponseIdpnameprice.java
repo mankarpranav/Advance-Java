c9009c8e6202c99472686dce99cd9ee76ea814d3
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-10 20:37:21 +0530

    Update ResponseIdpnameprice.java

--- Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/DTO/ResponseIdpnameprice.java
+++ Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/DTO/ResponseIdpnameprice.java
@@ -5,8 +5,8 @@ import lombok.Setter;
 
 @Setter
 @Getter
-public class ResponseIdpnameprice {
-	private String pname;
-	private int qty;
-	private double price;
+public class ResponseIdPnamePrice {
+    private String pname; // Represents the product name
+    private int qty; // Represents the quantity of the product
+    private double price; // Represents the price of the product
 }
