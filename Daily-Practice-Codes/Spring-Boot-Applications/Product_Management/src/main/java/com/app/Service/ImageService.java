b2a3c812929706863baf8d81551161e3931bfa61
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-10 20:40:24 +0530

    Update ImageService.java

--- Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/Service/ImageService.java
+++ Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/Service/ImageService.java
@@ -5,6 +5,7 @@ import org.springframework.web.multipart.MultipartFile;
 import com.app.DTO.ResponseDTO;
 
 public interface ImageService {
-
-	ResponseDTO insertImage(long id ,MultipartFile f);
+    
+    // Interface method to insert an image for a given id
+    ResponseDTO insertImage(long id, MultipartFile f);
 }
