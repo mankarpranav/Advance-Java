965c6958a99633ba340b16a6d7da891675f91284
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 01:33:28 +0530

    Update Server1.java

--- Daily-Practice-Codes/Project-3-(Lazy-Eager Loading)/src/main/java/pages/Server1.java
+++ Daily-Practice-Codes/Project-3-(Lazy-Eager Loading)/src/main/java/pages/Server1.java
@@ -9,28 +9,30 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value="/jump1",loadOnStartup = 1)			//will load eagerly
+@WebServlet(value = "/jump1", loadOnStartup = 1) // This servlet will load eagerly on application startup
 public class Server1 extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html");
-		try(PrintWriter pw = resp.getWriter()){
-			pw.print("jumped from index.html to server1 doGet()...");
-		}
-	}
+    // This method is called when a GET request is made to the specified URL ("/jump1").
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Set the response content type to HTML.
+        resp.setContentType("text/html");
+        
+        try (PrintWriter pw = resp.getWriter()) {
+            // Send the response containing the message to the client.
+            pw.print("Jumped from index.html to Server1's doGet()...");
+        }
+    }
 
-	@Override
-	public void destroy() {
-		System.out.println("in destroy of server1...");
-	}
+    // This method is called when the servlet is being destroyed.
+    @Override
+    public void destroy() {
+        System.out.println("In destroy of Server1...");
+    }
 
-	@Override
-	public void init() throws ServletException {
-		System.out.println("in init of server1...");
-	}
-	
-	
-	
-	
+    // This method is called when the servlet is being initialized.
+    @Override
+    public void init() throws ServletException {
+        System.out.println("In init of Server1...");
+    }
 }
