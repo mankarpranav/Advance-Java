d8b0a276de314b0f6b94a9f3dcda42af3f11f80a
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 01:34:30 +0530

    Update Server2.java

--- Daily-Practice-Codes/Project-3-(Lazy-Eager Loading)/src/main/java/pages/Server2.java
+++ Daily-Practice-Codes/Project-3-(Lazy-Eager Loading)/src/main/java/pages/Server2.java
@@ -9,25 +9,31 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/jump2")								//lazy loading
+@WebServlet("/jump2") // This servlet will load lazily on the first request to the URL ("/jump2")
 public class Server2 extends HttpServlet {
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setContentType("text/html");
-		try(PrintWriter pw = resp.getWriter()){
-			pw.print("jumped from index.html to server2 doGet()...");
-		}
-	}
+    // This method is called when a GET request is made to the specified URL ("/jump2").
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // Set the response content type to HTML.
+        resp.setContentType("text/html");
+        
+        try (PrintWriter pw = resp.getWriter()) {
+            // Send the response containing the message to the client.
+            pw.print("Jumped from index.html to Server2's doGet()...");
+        }
+    }
 
-	@Override
-	public void destroy() {
-		System.out.println("in destroy of server2...");
-	}
+    // This method is called when the servlet is being destroyed.
+    @Override
+    public void destroy() {
+        System.out.println("In destroy of Server2...");
+    }
 
-	@Override
-	public void init() throws ServletException {
-		System.out.println("in init of server2...");
-	}
+    // This method is called when the servlet is being initialized.
+    @Override
+    public void init() throws ServletException {
+        System.out.println("In init of Server2...");
+    }
 
 }
