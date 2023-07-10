a49556e83ee75fd5689c8e408a626fbc5fc7bd4f
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-10 20:37:56 +0530

    Update ProductException.java

--- Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/Exception/ProductException.java
+++ Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/Exception/ProductException.java
@@ -1,8 +1,8 @@
 package com.app.Exception;
 
 public class ProductException extends RuntimeException {
-
-	public ProductException(String msg) {
-		super(msg);
-	}
+    
+    public ProductException(String msg) {
+        super(msg); // Calls the constructor of the parent class (RuntimeException) with the specified error message
+    }
 }
