3573aca6d58f6da6b0cdae7b653fa89ce215380d
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-10 20:38:43 +0530

    Update GlobalProductExceptionHandler.java

--- Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/GobalExceptionHandler/GlobalProductExceptionHandler.java
+++ Daily-Practice-Codes/Spring-Boot-Applications/Product_Management/src/main/java/com/app/GobalExceptionHandler/GlobalProductExceptionHandler.java
@@ -15,17 +15,18 @@ import com.app.Exception.ProductException;
 @RestControllerAdvice
 public class GlobalProductExceptionHandler {
 
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> myhandler(MethodArgumentNotValidException m) {
-		Map<String, String> collect = m.getFieldErrors().stream()
-				.collect(Collectors.toMap(p -> p.getField(), p -> p.getDefaultMessage()));
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(collect);
-	}
-	
-	@ExceptionHandler(ProductException.class)
-	public ResponseEntity<?> myhandler1(ProductException p)
-	{
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO("error"));
-	}
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleMethodArgumentNotValid(MethodArgumentNotValidException ex) {
+        // Handling validation errors for request body fields
+        Map<String, String> errors = ex.getFieldErrors().stream()
+                .collect(Collectors.toMap(fieldError -> fieldError.getField(), fieldError -> fieldError.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
+    }
+
+    @ExceptionHandler(ProductException.class)
+    public ResponseEntity<?> handleProductException(ProductException ex) {
+        // Handling custom product-related exceptions
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ResponseDTO("error"));
+    }
 
 }
