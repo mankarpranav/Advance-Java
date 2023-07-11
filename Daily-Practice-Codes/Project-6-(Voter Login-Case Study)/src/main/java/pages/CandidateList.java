2d30e880334ee196583e8e43bcbf171ef0fc3f44
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:40:52 +0530

    Update CandidateList.java

--- Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/pages/CandidateList.java
+++ Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/pages/CandidateList.java
@@ -18,60 +18,30 @@ public class CandidateList extends HttpServlet {
 			throws ServletException, IOException {
 
 		response.setContentType("text/html");
-		
+
 		try (PrintWriter pw = response.getWriter()) {
-			
-			//{
-			//after cookie is added wala code
-			//API of HttpServletRequest : Cookie[] getCookes()
-			
-			//returns null if no cookie is sent
-			Cookie[] cookies=request.getCookies();
-			if(cookies != null) {
-				//how to retrieve data from cookie using getname and getvalue methods of cookie class
-				
-				Cookie[] cookies1 = request.getCookies(); // Get the cookies from the request
 
-				if (cookies1 != null) {
-				    for (Cookie c : cookies1) { //=> cookie are sent from the clnt
-				    	//getName, getValue : 
-				        if (c.getName().equals("user_dtls")) {
-				            String cookieValue = c.getValue(); // Retrieve the value of the cookie
-				           // Process the cookie value as needed
-				           
-				            pw.print("<h5>aaaaaValidated user details</h5> "+cookieValue);
-				            
-				            break; // Exit the loop since we found the desired cookie
-				        }
-				    }
+			// Cookie Retrieval
+			Cookie[] cookies = request.getCookies(); // Get the cookies from the request
+			if (cookies != null) {
+				for (Cookie c : cookies) {
+					if (c.getName().equals("user_dtls")) {
+						String cookieValue = c.getValue(); // Retrieve the value of the cookie
+						pw.print("<h5>Validated user details: " + cookieValue + "</h5>");
+						break; // Exit the loop since we found the desired cookie
+					}
 				}
-
-				
-				
-				
-			}else {
-				pw.print("<h5>Session tracking failed, no cookies!!!! Found!!!<h5>");
+			} else {
+				pw.print("<h5>Session tracking failed, no cookies found!</h5>");
 			}
-			//}
-			
+
 			pw.print("<h5>In candidate list page ....</h5>");
-			pw.print("<h5> Validated user details " + request.getParameter("em") + "</h5>");
-			//in above line - output --> null
-			//client submitted form
-			//req resp loop got over
-			//working with stateless protocol
-			//every req is fresh req.
-			//so everytime it is new req and thus
-			//it shows null as it doesnt have anything store beforehand
-			//and thus we are going to learn session in next lecture.
-			
-			
+			pw.print("<h5>Validated user details: " + request.getParameter("em") + "</h5>");
+			// The above line shows null because it's a new request and no parameter named
+			// "em" was passed in the request. We will learn about session tracking in the
+			// next lecture.
+
 		}
 	}
 
 }
-
-//when needed one time jobs to be done then write/override init() destroy() methods
-//otherwise no need.
-
-//ccokie class - 2methods --> 1.2.
