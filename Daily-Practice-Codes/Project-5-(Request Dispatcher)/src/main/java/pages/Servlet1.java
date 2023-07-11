c6d849b82de7cea59559996528543e57a2f06d39
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:35:05 +0530

    Update Servlet1.java

--- Daily-Practice-Codes/Project-5-(Request Dispatcher)/src/main/java/pages/Servlet1.java
+++ Daily-Practice-Codes/Project-5-(Request Dispatcher)/src/main/java/pages/Servlet1.java
@@ -8,29 +8,28 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class Servlet1 extends HttpServlet {
-private static final long serialVersionUID = 1L;
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		int a = Integer.parseInt(request.getParameter("num1"));
-		int b = Integer.parseInt(request.getParameter("num2"));
-		int result = a+b;
-		
-		//Can we call servlet from another servlet ?? YES
-		//using two ways -
-		
-		//1.Request Dispatcher and 2.Redirect
-		
-		//to send value we can send by-
-		//1.setting attribute in responce object
-		//2.by using session management
-		
-		request.setAttribute("result",result);		//sending in key and value format
-		
-		//creating object of RequestDispatcher which is an interface,
-		//create object sing getRequestDispatcher
-		RequestDispatcher rd = request.getRequestDispatcher("sq"); 
-		rd.forward(request, response);
-	}
+    private static final long serialVersionUID = 1L;
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int a = Integer.parseInt(request.getParameter("num1"));
+        int b = Integer.parseInt(request.getParameter("num2"));
+        int result = a + b;
+
+        // We can call a servlet from another servlet using two ways:
+        // 1. Request Dispatcher
+        // 2. Redirect
+
+        // To send values between servlets, we can use:
+        // 1. Setting attribute in the response object
+        // 2. Session management
+
+        request.setAttribute("result", result); // Sending the result value as an attribute
+
+        // Creating an object of RequestDispatcher which is an interface,
+        // obtained using getRequestDispatcher
+        RequestDispatcher rd = request.getRequestDispatcher("sq");
+        rd.forward(request, response);
+    }
 }
