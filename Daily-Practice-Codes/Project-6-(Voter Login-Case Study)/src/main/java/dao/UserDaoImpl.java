42466b9ddb4856fff72276820718efb401ec2cbd
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:39:01 +0530

    Update UserDaoImpl.java

--- Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/dao/UserDaoImpl.java
+++ Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/dao/UserDaoImpl.java
@@ -6,44 +6,47 @@ import static utils.DBUtils.*;
 import pojos.User;
 
 public class UserDaoImpl implements UserDao {
-	
-	// fields
-	private Connection cn;
-	private PreparedStatement pst1;
 
-	public UserDaoImpl() throws SQLException {
-		// open conn
-		
-		cn = openConnection();
-		//avoid sequel injection and repeatitiveness -- using prepareStatement
-		
-		pst1 = cn.prepareStatement("select * from users where email=? and password=?");
-		System.out.println("user dao created!");
-	}
+    // Fields
+    private Connection cn;
+    private PreparedStatement pst1;
 
-	@Override
-	public User authenticateUser(String email, String password) throws SQLException {
-		// set IN params
-		pst1.setString(1, email);		
-		pst1.setString(2, password);
-		try(ResultSet rst=pst1.executeQuery()) {
-			if(rst.next()) //=> success
-				/*
-				 * int id, String firstName, String lastName, String email, String password, Date dob,
-			boolean votingStatus, String role
-				 */
-				//1.2.3.... as per the result set order
-				return new User(rst.getInt(1), rst.getString(2), rst.getString(3), email, password, 
-						rst.getDate(6), rst.getBoolean(7), rst.getString(8));
-		}//rst.close() <-- auto closable
-		return null;
-	}
+    public UserDaoImpl() throws SQLException {
+        // Open connection
+        cn = openConnection();
+        
+        // Avoid SQL injection and repetitiveness by using PreparedStatement
+        pst1 = cn.prepareStatement("select * from users where email=? and password=?");
+        System.out.println("UserDaoImpl created!");
+    }
 
-	public void cleanUp() throws SQLException {
-		if (pst1 != null)
-			pst1.close();
-		closeConnection();
-		System.out.println("user dao cleaned up!");
-	}
+    @Override
+    public User authenticateUser(String email, String password) throws SQLException {
+        // Set IN parameters
+        pst1.setString(1, email);
+        pst1.setString(2, password);
+        
+        try (ResultSet rst = pst1.executeQuery()) {
+            if (rst.next()) {
+                // Success
+                /*
+                 * int id, String firstName, String lastName, String email, String password, Date dob,
+                 * boolean votingStatus, String role
+                 */
+                // Create a new User object using the values from the result set
+                return new User(rst.getInt(1), rst.getString(2), rst.getString(3), email, password,
+                        rst.getDate(6), rst.getBoolean(7), rst.getString(8));
+            }
+        } // ResultSet automatically closed due to try-with-resources
+        
+        return null;
+    }
+
+    public void cleanUp() throws SQLException {
+        if (pst1 != null)
+            pst1.close();
+        closeConnection();
+        System.out.println("UserDaoImpl cleaned up!");
+    }
 
 }
