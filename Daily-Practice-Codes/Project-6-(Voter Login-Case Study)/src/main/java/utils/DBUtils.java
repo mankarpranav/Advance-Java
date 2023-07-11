5c559d351dea898d5be14d72200d6ae49653df56
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:50:10 +0530

    Update DBUtils.java

--- Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/utils/DBUtils.java
+++ Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/utils/DBUtils.java
@@ -1,31 +1,40 @@
 package utils;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBUtils {
 	private static Connection cn;
 
-//http://localhost:8080/day2.1/candidate_list --> means 2 req coming from client
-	
-//open connection
+	/**
+	 * Open a database connection
+	 * 
+	 * @return the opened Connection object
+	 * @throws SQLException if a database access error occurs
+	 */
 	public static Connection openConnection() throws SQLException {
 		String dbURL = "jdbc:mysql://localhost:3306/advjava?createDatabaseIfNotExist=true&useSSL=false&allowPublicKeyRetrieval=true";
-		//creating connection -- db independent
-		
+		// Creating connection -- DB independent
+
 		cn = DriverManager.getConnection(dbURL, "root", "root");
-		//100 separate connections will be created if called 100 times ; 
-		
-		//4-types of drivers -> 4th type of driver we are using
-		
-		System.out.println("db cn established....");
+		// 100 separate connections will be created if called 100 times
+
+		// 4 types of drivers -> 4th type of driver we are using
+
+		System.out.println("Database connection established.");
 		return cn;
-		//returning connection
+		// Returning the Connection object
 	}
 
-//close connection
-	public static void closeConnection() throws SQLException{
-		if(cn != null)
+	/**
+	 * Close the database connection
+	 * 
+	 * @throws SQLException if a database access error occurs
+	 */
+	public static void closeConnection() throws SQLException {
+		if (cn != null)
 			cn.close();
-		System.out.println("db cn closed....");
+		System.out.println("Database connection closed.");
 	}
 }
