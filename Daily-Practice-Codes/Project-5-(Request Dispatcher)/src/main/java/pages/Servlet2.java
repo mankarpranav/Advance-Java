be95a011fe8537bdfcaa384de9ccba1ff5b05765
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:35:46 +0530

    Update Servlet2.java

--- Daily-Practice-Codes/Project-5-(Request Dispatcher)/src/main/java/pages/Servlet2.java
+++ Daily-Practice-Codes/Project-5-(Request Dispatcher)/src/main/java/pages/Servlet2.java
@@ -4,24 +4,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet2 extends HttpServlet {
-private static final long serialVersionUID = 1L;
-	
-	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter pw = response.getWriter() ;
-		pw.print("in 2nd servlet..");
-		
-		//Fetching value which is fetched from servlet1
-		
-		int num = (int) request.getAttribute("result");
-		int square = num*num;
-		
-		PrintWriter pw1 = response.getWriter();
-		pw1.print("\n\nSquare of number after multiplication is :"+square);
-	}
+    private static final long serialVersionUID = 1L;
+
+    protected void service(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        PrintWriter pw = response.getWriter();
+        pw.print("In 2nd servlet..");
+
+        // Fetching the value which was set in Servlet1
+
+        int num = (int) request.getAttribute("result");
+        int square = num * num;
+
+        PrintWriter pw1 = response.getWriter();
+        pw1.print("\n\nSquare of the number after multiplication is: " + square);
+    }
 }
