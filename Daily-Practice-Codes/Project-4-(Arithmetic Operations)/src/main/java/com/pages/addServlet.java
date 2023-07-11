216238c177e3039694920ca661a5ca0e92c60828
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:33:11 +0530

    Update addServlet.java

--- Daily-Practice-Codes/Project-4-(Arithmetic Operations)/src/main/java/com/pages/addServlet.java
+++ Daily-Practice-Codes/Project-4-(Arithmetic Operations)/src/main/java/com/pages/addServlet.java
@@ -8,24 +8,21 @@ import javax.servlet.http.HttpServletResponse;
 
 public class addServlet extends HttpServlet {
 
-	public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException{
-		int a = Integer.parseInt(req.getParameter("num1"));
-		int b = Integer.parseInt(req.getParameter("num2"));
-		
-		int addition = a+b;
-		int substraction = a-b;
-		int multiplication = a*b;
-		int division = a/b;
-		//System.out.println("Result is : "+ result);
-		//This will print on console.
-		
-		//We want to Print result on screen
-		//getWriter() throws exception to handle that we will put that in try block or delegate to the caller
-		
-		resp.getWriter().println("result is of "+a+" + "+b+" is "+ addition);
-		resp.getWriter().println("result is of "+a+" - "+b+" is "+ substraction);
-		resp.getWriter().println("result is of "+a+" * "+b+" is "+ multiplication);
-		resp.getWriter().println("result is of "+a+" / "+b+" is "+ division);
-		
-	} 
+    public void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        // Retrieving the values of "num1" and "num2" parameters from the request and parsing them as integers
+        int a = Integer.parseInt(req.getParameter("num1"));
+        int b = Integer.parseInt(req.getParameter("num2"));
+
+        // Performing arithmetic operations
+        int addition = a + b;
+        int subtraction = a - b;
+        int multiplication = a * b;
+        int division = a / b;
+
+        // Printing the results on the response
+        resp.getWriter().println("Result of " + a + " + " + b + " is " + addition);
+        resp.getWriter().println("Result of " + a + " - " + b + " is " + subtraction);
+        resp.getWriter().println("Result of " + a + " * " + b + " is " + multiplication);
+        resp.getWriter().println("Result of " + a + " / " + b + " is " + division);
+    }
 }
