0ceef998a89ee69912192d4f193a574ea7c48b22
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-11 23:38:11 +0530

    Update UserDao.java

--- Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/dao/UserDao.java
+++ Daily-Practice-Codes/Project-6-(Voter Login-Case Study)/src/main/java/dao/UserDao.java
@@ -5,26 +5,25 @@ import java.sql.SQLException;
 import pojos.User;
 
 public interface UserDao {
-//add a method for user authentication(login)
-	User authenticateUser(String email, String password) throws SQLException;
+    // Add a method for user authentication (login)
+    User authenticateUser(String email, String password) throws SQLException;
 }
 
+// HOMEWORK:
 
-//HOMEWORK :
+// In the dao interface, add a new method for user registration
+// The return type for DML (Data Manipulation Language) is String
 
-//dao interface --> new method --> register(user obj)
-//return type for dml is string 
+// Implementation class - use PreparedStatement (pst2)
+// Set input parameters except for ID, status, and role
+// Execute the update using executeUpdate method
+// Return the row count as a String
 
-//imple class --> pst2
-//set in params -- expcept id status roll
-//excuteUpdate
-//returns row count
+// Initialize the Dao instance in the init method
+// Clean up resources in the destroy method
+// Handle doPost method - set the content type and get PrintWriter
 
-//init --> dai instance
-//destroy --> cleanUp
-//doPost --> setContent, printWriter
+// Read parameters from the request
 
-//read parameters
-//
-//
-//
+// Implement the registration logic inside the doPost method
+// Use the UserDao instance to register the user by invoking the newly added method
