0dc19c76ccf8d06fb5c255d759c1ac347af745d3
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-13 23:51:03 +0530

    Update Servlet2.java

--- Daily-Practice-Codes/Project-7-(URL Rewriting)/src/main/java/pages/Servlet2.java
+++ Daily-Practice-Codes/Project-7-(URL Rewriting)/src/main/java/pages/Servlet2.java
@@ -9,20 +9,20 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class Servlet2 extends HttpServlet {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//		PrintWriter pw = response.getWriter() ;
-//		pw.print("in 2nd servlet..");
-//		
-//		//now wc will redirect here at servlet2
-//		
+		// Retrieve the value passed from the first servlet
 		int num = Integer.parseInt(request.getParameter("k"));
-		int square = num*num;
 		
-		PrintWriter pw1 = response.getWriter();
-		pw1.print("\n\nSquare of number "+num+" after multiplication is :"+square);
-	
+		// Calculate the square of the number
+		int square = num * num;
+		
+		// Get the PrintWriter object to write the response
+		PrintWriter pw = response.getWriter();
+		pw.print("\n\nSquare of number " + num + " after multiplication is: " + square);
+		
+		// Print a message to the console
 		System.out.println("Here on Second Screen!!");
 	}
 }
