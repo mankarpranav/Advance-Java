09fd83e128dc4815961f964442751c482a68de78
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-13 23:56:15 +0530

    Update Servlet2.java

--- Daily-Practice-Codes/Project-8-(HttpSession )/src/main/java/pages/Servlet2.java
+++ Daily-Practice-Codes/Project-8-(HttpSession )/src/main/java/pages/Servlet2.java
@@ -10,21 +10,21 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class Servlet2 extends HttpServlet {
-private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter pw = response.getWriter() ;
+		PrintWriter pw = response.getWriter();
 		pw.print("in 2nd servlet..");
 		
-		//Now, we will get session and perform operations as we want.
-		
+		// Get the session and retrieve the stored result
 		HttpSession session = request.getSession();
+		int res = (int) session.getAttribute("result");
 		
-		int res =(int)(session.getAttribute("result")); 	
+		// Perform the square operation on the result
+		int square = res * res;
 		
-		int square = res*res;
-		
+		// Print the square value to the response
 		PrintWriter pw1 = response.getWriter();
-		pw1.print("\n\nSquare of number after multiplication is :"+square);
+		pw1.print("\n\nSquare of number after multiplication is: " + square);
 	}
 }
