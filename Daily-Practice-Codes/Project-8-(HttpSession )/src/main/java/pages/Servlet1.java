15434d5e42305095024823760a42c55e96b7e526
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-13 23:55:29 +0530

    Update Servlet1.java

--- Daily-Practice-Codes/Project-8-(HttpSession )/src/main/java/pages/Servlet1.java
+++ Daily-Practice-Codes/Project-8-(HttpSession )/src/main/java/pages/Servlet1.java
@@ -8,28 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 public class Servlet1 extends HttpServlet {
-private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;
+
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// Extract the values of the submitted numbers
 		int a = Integer.parseInt(request.getParameter("num1"));
 		int b = Integer.parseInt(request.getParameter("num2"));
-		int result = a+b;
 		
-		//to send value we can send by-
-		//1.setting attribute in responce object
-		//2.by using session management
+		// Perform the addition operation
+		int result = a + b;
 		
-		//Here using session management..
-		//creating object of HTTPSession which is actually interface 
-		//so creating object of class implementing HttpSession interface
-		
+		// Use session management to store the result value
 		HttpSession session = request.getSession();
+		session.setAttribute("result", result);
 		
-		//setting attributes in the form of key and value pair.
-		session.setAttribute("result",result);
-		
+		// Redirect the response to another servlet named "sq"
 		response.sendRedirect("sq");
 	}
 }
