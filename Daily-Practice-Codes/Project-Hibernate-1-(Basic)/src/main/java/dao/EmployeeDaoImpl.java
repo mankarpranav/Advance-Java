2b8b1d1ef84beb8ea2b8d3e401e94b55152d3bc7
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-14 23:52:38 +0530

    Update EmployeeDaoImpl.java

--- Daily-Practice-Codes/Project-Hibernate-1-(Basic)/src/main/java/dao/EmployeeDaoImpl.java
+++ Daily-Practice-Codes/Project-Hibernate-1-(Basic)/src/main/java/dao/EmployeeDaoImpl.java
@@ -7,24 +7,31 @@ import org.hibernate.Transaction;
 
 import pojos.Employee;
 
-public class EmployeeDaoImpl implements EmployeeDao{
+public class EmployeeDaoImpl implements EmployeeDao {
 
-	@Override
-	public void insertEmployee(Employee emp) {
-		
-		Session ss = getFactory().openSession();
-		Transaction tx = ss.beginTransaction();
-		
-		try{
-			System.out.println("Done! Inserted Successfully!!");
-			ss.save(emp);
-			tx.commit();
-		}catch(Exception e){
-			tx.rollback();
-			System.out.println("Error in Transaction"+e);
-		}finally {
-			if(ss!=null)
-				ss.close();
-		}
-	}	
+    @Override
+    public void insertEmployee(Employee emp) {
+        // Open a new session from the Hibernate session factory
+        Session ss = getFactory().openSession();
+
+        // Begin a new transaction
+        Transaction tx = ss.beginTransaction();
+
+        try {
+            System.out.println("Done! Inserted Successfully!!");
+            // Save the employee object to the database
+            ss.save(emp);
+
+            // Commit the transaction
+            tx.commit();
+        } catch (Exception e) {
+            // Roll back the transaction in case of an error
+            tx.rollback();
+            System.out.println("Error in Transaction" + e);
+        } finally {
+            // Close the session
+            if (ss != null)
+                ss.close();
+        }
+    }
 }
