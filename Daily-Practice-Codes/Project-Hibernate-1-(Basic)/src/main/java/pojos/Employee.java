c2a8bea64163159e764bddb9b559d630efba7737
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-14 23:53:59 +0530

    Update Employee.java

--- Daily-Practice-Codes/Project-Hibernate-1-(Basic)/src/main/java/pojos/Employee.java
+++ Daily-Practice-Codes/Project-Hibernate-1-(Basic)/src/main/java/pojos/Employee.java
@@ -11,127 +11,119 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
-@Table(name="EmployeeTable")
+@Table(name = "EmployeeTable")
 public class Employee {
-	
-	/*
-	 * emp_id(PK) ,first_name,last_name,email(unique),password,join_date,
-	 * emp_type(full_time/part_time/contract...) salary
-	 */
-	
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int empID;
-	
-	@Column(name="FirstName",length=30)
-	private String fname;
-	
-	@Column(name="LastName",length=30)
-	private String lname;
-	
-	@Column(name="EmailAddress",length=30)
-	private String email;
-	
-	@Column(name="Password",length=30,nullable=false)
-	private String password;
-	
-	@Column(name="JoiningDate")
-	private LocalDate joinDate;
-	
-	@Column(name="TypeofContract")
-	@Enumerated(EnumType.STRING)
-	private EmployeeType type;
-	
-	@Column(name="Salary")
-	private double salary;
-	
-	public Employee(String fname, String lname, String email, String password, LocalDate joinDate,
-			EmployeeType type, double salary) {
-		super();
-		this.fname = fname;
-		this.lname = lname;
-		this.email = email;
-		this.password = password;
-		this.joinDate = joinDate;
-		this.type = type;
-		this.salary = salary;
-	}
+    // emp_id(PK), first_name, last_name, email(unique), password, join_date,
+    // emp_type(full_time/part_time/contract...), salary
 
-	public Employee() {
-		super();
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int empID;
 
-	@Override
-	public String toString() {
-		return "Employee [empID=" + empID + ", fname=" + fname + ", lname=" + lname + ", email=" + email + ", joinDate="
-				+ joinDate + ", type=" + type + ", salary=" + salary + "]";
-	}
+    @Column(name = "FirstName", length = 30)
+    private String fname;
 
-	public int getEmpID() {
-		return empID;
-	}
+    @Column(name = "LastName", length = 30)
+    private String lname;
 
-	public void setEmpID(int empID) {
-		this.empID = empID;
-	}
+    @Column(name = "EmailAddress", length = 30)
+    private String email;
 
-	public String getFname() {
-		return fname;
-	}
+    @Column(name = "Password", length = 30, nullable = false)
+    private String password;
 
-	public void setFname(String fname) {
-		this.fname = fname;
-	}
+    @Column(name = "JoiningDate")
+    private LocalDate joinDate;
 
-	public String getLname() {
-		return lname;
-	}
+    @Column(name = "TypeofContract")
+    @Enumerated(EnumType.STRING)
+    private EmployeeType type;
 
-	public void setLname(String lname) {
-		this.lname = lname;
-	}
+    @Column(name = "Salary")
+    private double salary;
 
-	public String getEmail() {
-		return email;
-	}
+    public Employee(String fname, String lname, String email, String password, LocalDate joinDate, EmployeeType type,
+            double salary) {
+        this.fname = fname;
+        this.lname = lname;
+        this.email = email;
+        this.password = password;
+        this.joinDate = joinDate;
+        this.type = type;
+        this.salary = salary;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public Employee() {
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    @Override
+    public String toString() {
+        return "Employee [empID=" + empID + ", fname=" + fname + ", lname=" + lname + ", email=" + email
+                + ", joinDate=" + joinDate + ", type=" + type + ", salary=" + salary + "]";
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public int getEmpID() {
+        return empID;
+    }
 
-	public LocalDate getJoinDate() {
-		return joinDate;
-	}
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
 
-	public void setJoinDate(LocalDate joinDate) {
-		this.joinDate = joinDate;
-	}
+    public String getFname() {
+        return fname;
+    }
 
-	public EmployeeType getType() {
-		return type;
-	}
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
 
-	public void setType(EmployeeType type) {
-		this.type = type;
-	}
+    public String getLname() {
+        return lname;
+    }
 
-	public double getSalary() {
-		return salary;
-	}
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
 
-	public void setSalary(double salary) {
-		this.salary = salary;
-	}
-	
-	
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public void setJoinDate(LocalDate joinDate) {
+        this.joinDate = joinDate;
+    }
+
+    public EmployeeType getType() {
+        return type;
+    }
+
+    public void setType(EmployeeType type) {
+        this.type = type;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
 }
