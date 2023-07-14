ba2b28f91e55445db8847d0375250088d2928887
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-14 23:56:17 +0530

    Update Test.java

--- Daily-Practice-Codes/Project-Hibernate-1-(Basic)/src/main/java/tester/Test.java
+++ Daily-Practice-Codes/Project-Hibernate-1-(Basic)/src/main/java/tester/Test.java
@@ -13,25 +13,40 @@ import pojos.EmployeeType;
 
 public class Test {
 
-	public static void main(String[] args) {
-		
-		try(SessionFactory retFactory = getFactory();
-				Scanner sc=new Scanner(System.in)) {
-			System.out.println("<------HIBERNATE WORKING SMOOTH------>");
-			
-			Employee emp = new Employee(sc.next(),
-					sc.next(),
-					sc.next(),
-					sc.next(),
-					LocalDate.parse(sc.next()),
-					EmployeeType.valueOf(sc.next().toUpperCase()),
-					sc.nextDouble());
-			
-			EmployeeDaoImpl obj = new EmployeeDaoImpl();
-			obj.insertEmployee(emp);
-			
-		}catch(Exception e) {
-			System.out.println("Message "+e);
-		}
-	}
+    public static void main(String[] args) {
+        try (SessionFactory retFactory = getFactory(); Scanner sc = new Scanner(System.in)) {
+            System.out.println("<------HIBERNATE WORKING SMOOTH------>");
+
+            // Prompt user to enter employee details
+            System.out.print("Enter First Name: ");
+            String fname = sc.next();
+
+            System.out.print("Enter Last Name: ");
+            String lname = sc.next();
+
+            System.out.print("Enter Email: ");
+            String email = sc.next();
+
+            System.out.print("Enter Password: ");
+            String password = sc.next();
+
+            System.out.print("Enter Joining Date (yyyy-mm-dd): ");
+            LocalDate joinDate = LocalDate.parse(sc.next());
+
+            System.out.print("Enter Employee Type (FULLTIME/PARTTIME/INTERN): ");
+            EmployeeType type = EmployeeType.valueOf(sc.next().toUpperCase());
+
+            System.out.print("Enter Salary: ");
+            double salary = sc.nextDouble();
+
+            // Create an employee object
+            Employee emp = new Employee(fname, lname, email, password, joinDate, type, salary);
+
+            // Insert the employee into the database
+            EmployeeDaoImpl obj = new EmployeeDaoImpl();
+            obj.insertEmployee(emp);
+        } catch (Exception e) {
+            System.out.println("Message " + e);
+        }
+    }
 }
