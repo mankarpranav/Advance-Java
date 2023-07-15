16c8b67c642d98a9f9bbcad88b5f56e0b840f830
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-15 23:55:39 +0530

    Update Course.java

--- Daily-Practice-Codes/Spring-Boot-Applications/Student_Admin_Management/src/main/java/pojos/Course.java
+++ Daily-Practice-Codes/Spring-Boot-Applications/Student_Admin_Management/src/main/java/pojos/Course.java
@@ -1,15 +1,23 @@
 package pojos;
 
 public enum Course {
-	DAC(85), DBDA(80), DITISS(78), DAI(88);
+    DAC(85),
+    DBDA(80),
+    DITISS(78),
+    DAI(88);
 
-	private int minScore;
+    private int minScore;
 
-	private Course(int minScore) {
-		this.minScore = minScore;
-	}
-	public int getMinScore() {
-		return minScore;
-	}
+    private Course(int minScore) {
+        this.minScore = minScore;
+    }
 
+    /**
+     * Get the minimum score required for the course.
+     *
+     * @return the minimum score required
+     */
+    public int getMinScore() {
+        return minScore;
+    }
 }
