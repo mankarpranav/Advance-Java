af8fabd49f495c56f04ee454a89f780d935f44ca
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-15 23:52:28 +0530

    Update SpringProductApplication1Application.java

--- Daily-Practice-Codes/Project-Spring_Product_Application-1/src/main/java/com/example/demo/SpringProductApplication1Application.java
+++ Daily-Practice-Codes/Project-Spring_Product_Application-1/src/main/java/com/example/demo/SpringProductApplication1Application.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main class for the Spring Boot application.
+ */
 @SpringBootApplication
 public class SpringProductApplication1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringProductApplication1Application.class, args);
-	}
+    /**
+     * The entry point of the application.
+     *
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProductApplication1Application.class, args);
+    }
 
 }
