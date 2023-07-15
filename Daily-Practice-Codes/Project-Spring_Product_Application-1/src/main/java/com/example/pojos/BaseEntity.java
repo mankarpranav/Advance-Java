a12e1b0adbcd9a5c749da1ec9a72ab3198ca6b90
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-15 23:52:58 +0530

    Update BaseEntity.java

--- Daily-Practice-Codes/Project-Spring_Product_Application-1/src/main/java/com/example/pojos/BaseEntity.java
+++ Daily-Practice-Codes/Project-Spring_Product_Application-1/src/main/java/com/example/pojos/BaseEntity.java
@@ -5,20 +5,24 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 
+/**
+ * Base entity class for other entities in the application.
+ */
 @MappedSuperclass
 public class BaseEntity {
-	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private Long id;
-	public BaseEntity() {
-		System.out.println("in base ctr...");
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    public BaseEntity() {
+        System.out.println("In BaseEntity constructor...");
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
