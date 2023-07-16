91fd2fc6c66fac8301c06eea2266692330478afb
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-16 23:44:39 +0530

    Update Laptop.java

--- Daily-Practice-Codes/SpringProject2_DependecyInjection/src/main/java/com/example/demo/Laptop.java
+++ Daily-Practice-Codes/SpringProject2_DependecyInjection/src/main/java/com/example/demo/Laptop.java
@@ -4,38 +4,37 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class Laptop {
-	private int laptopId;
-	private String laptopName;
+    private int laptopId;
+    private String laptopName;
 
-	public int getLaptopId() {
-		return laptopId;
-	}
+    public int getLaptopId() {
+        return laptopId;
+    }
 
-	public void setLaptopId(int laptopId) {
-		this.laptopId = laptopId;
-	}
+    public void setLaptopId(int laptopId) {
+        this.laptopId = laptopId;
+    }
 
-	public String getLaptopName() {
-		return laptopName;
-	}
+    public String getLaptopName() {
+        return laptopName;
+    }
 
-	public void setLaptopName(String laptopName) {
-		this.laptopName = laptopName;
-	}
+    public void setLaptopName(String laptopName) {
+        this.laptopName = laptopName;
+    }
 
-	public Laptop() {
-		super();
-	}
+    public Laptop() {
+        super();
+    }
 
-	public Laptop(int laptopId, String laptopName) {
-		super();
-		this.laptopId = laptopId;
-		this.laptopName = laptopName;
-	}
+    public Laptop(int laptopId, String laptopName) {
+        super();
+        this.laptopId = laptopId;
+        this.laptopName = laptopName;
+    }
 
-	// custom method
-	public void compile() {
-		System.out.println("compiling...");
-	}
-
+    // Custom method
+    public void compile() {
+        System.out.println("Compiling...");
+    }
 }
