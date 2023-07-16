164ece0f322812f13b2284ad5f1f19d65e7302f2
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-16 23:40:30 +0530

    Update SpringProject1BasicHelloWorldApplication.java

--- Daily-Practice-Codes/SpringProject1_BasicHelloWorld/src/main/java/com/example/demo/SpringProject1BasicHelloWorldApplication.java
+++ Daily-Practice-Codes/SpringProject1_BasicHelloWorld/src/main/java/com/example/demo/SpringProject1BasicHelloWorldApplication.java
@@ -6,40 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringProject1BasicHelloWorldApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringProject1BasicHelloWorldApplication.class, args);
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProject1BasicHelloWorldApplication.class, args);
 
-		System.out.println("Hello World from Spring Boot Application!!!!!!!!");
-	}
+        System.out.println("Hello World from Spring Boot Application!!!!!!!!");
+    }
 
 }
-
-/**
- * 
- * 
- * . ____ _ __ _ _ /\\ / ___'_ __ _ _(_)_ __ __ _ \ \ \ \ ( ( )\___ | '_ | '_| |
- * '_ \/ _` | \ \ \ \ \\/ ___)| |_)| | | | | || (_| | ) ) ) ) ' |____| .__|_|
- * |_|_| |_\__, | / / / / =========|_|==============|___/=/_/_/_/ [32m ::
- * Spring Boot :: [39m [2m (v2.7.13)[0;39m
- * 
- * [2m2023-06-25 11:48:27.300[0;39m [32m INFO[0;39m [35m5300[0;39m
- * [2m---[0;39m [2m[ main][0;39m
- * [36mSpringProject1BasicHelloWorldApplication[0;39m [2m:[0;39m Starting
- * SpringProject1BasicHelloWorldApplication using Java 20.0.1 on DESKTOP-OE9L8AE
- * with PID 5300
- * (F:\Daily-Practice-Codes\SpringProject1_BasicHelloWorld\target\classes
- * started by Windows 10 in
- * F:\Daily-Practice-Codes\SpringProject1_BasicHelloWorld) [2m2023-06-25
- * 11:48:27.306[0;39m [32m INFO[0;39m [35m5300[0;39m [2m---[0;39m [2m[
- * main][0;39m [36mSpringProject1BasicHelloWorldApplication[0;39m
- * [2m:[0;39m No active profile set, falling back to 1 default profile:
- * "default" [2m2023-06-25 11:48:28.345[0;39m [32m INFO[0;39m
- * [35m5300[0;39m [2m---[0;39m [2m[ main][0;39m
- * [36mSpringProject1BasicHelloWorldApplication[0;39m [2m:[0;39m Started
- * SpringProject1BasicHelloWorldApplication in 1.683 seconds (JVM running for
- * 3.208)
- * 
- * Hello World from Spring Boot Application!!!!!!!!
- * 
- * 
- */
