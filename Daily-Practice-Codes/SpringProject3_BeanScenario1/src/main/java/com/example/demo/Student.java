0a8df961c6c24284520d9fa764eb55b0357ec497
Pranav Mankar <60339881+mankarpranav@users.noreply.github.com>
2023-07-17 23:42:35 +0530

    Update Student.java

--- Daily-Practice-Codes/SpringProject3_BeanScenario1/src/main/java/com/example/demo/Student.java
+++ Daily-Practice-Codes/SpringProject3_BeanScenario1/src/main/java/com/example/demo/Student.java
@@ -5,29 +5,28 @@ import org.springframework.stereotype.Component;
 @Component
 public class Student {
 
-	private int id;
-	private String name;
+    private int id;
+    private String name;
 
-	// constructor
-	Student() {
-		System.out.println("In Student Constructor...");
-	}
+    // Constructor
+    public Student() {
+        System.out.println("In Student Constructor...");
+    }
 
-	// getter n setter
-	public int getId() {
-		return id;
-	}
+    // Getter and Setter methods
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
+    public void setName(String name) {
+        this.name = name;
+    }
 }
